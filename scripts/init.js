e16e579b1b961174da4a478f21ccf2ae412378c8
Oscar Lopez <oscarlopez@despegar.com>
2016-11-25 13:15:48 -0300

    init script. Insert hotels into db

--- scripts/init.js
+++ scripts/init.js
@@ -2,28 +2,41 @@
 
 const config = require('config');
 const mongojs = require('mongojs');
+console.log(config);
 const db = mongojs(config.get('db.stringconn'), config.get('db.collections'));
 
-db.hotels.insert([
-  {
-    'name': 'Hotel Emperador',
-    'stars': '3',
-    'price': '1596'
-  },
-  {
-    'name': 'Petit Palace San Bernardo',
-    'stars': '4',
-    'price': '2145'
-  },
-  {
-    'name': 'Hotel Nuevo Boston',
-    'stars': '2',
-    'price': '861'
-  }
-], function (err, result) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(result);
-  }
+db.hotels.drop(function(err,result){
+    if(err){
+        console.log('Error drop hotels collection' ,err);
+    }else{
+        insertHotels();
+    }
 });
+
+function insertHotels (){
+    db.hotels.insert([
+        {
+            'name': 'Hotel Emperador',
+            'stars': '3',
+            'price': '1596'
+        },
+        {
+            'name': 'Petit Palace San Bernardo',
+            'stars': '4',
+            'price': '2145'
+        },
+        {
+            'name': 'Hotel Nuevo Boston',
+            'stars': '2',
+            'price': '861'
+        }
+    ], function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+        }
+    });
+
+}
+
