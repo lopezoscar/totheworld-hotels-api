4c4551acb7e973b81506576e1e4fa354d8dd1cdb
Oscar Lopez <oscarlopez@despegar.com>
2016-11-25 13:59:47 -0300

    v2 for hotels

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const express = require('express');
 const app = express();
 
 app.use('/v1/hotels', require('./routes/v1/hotels'));
+app.use('/v2/hotels', require('./routes/v2/hotels'));
 
 module.exports = app;
