e9953818a8bbbec06026f40128b4d2a2f66ad383
Oscar Lopez <oscarlopez@despegar.com>
2016-11-25 14:01:04 -0300

    v1 y v2 /hotels

--- routes/v2/hotels.js
+++ routes/v2/hotels.js
@@ -6,7 +6,13 @@ const hotels = require('../../lib/hotels');// TODO Cambiar pasaje de modules.
 
 router.get('/', function index (req, res) {
 
-  hotels.findHotels()
+  // Todo validate input
+  let limit = Number(req.query.limit) || 10;
+  let page = Number(req.query.page) || 1;
+  let filters = {};
+  let sort = {};
+
+  hotels.getHotels({limit, page}, filters, sort)
         .then(hotels => res.json(hotels))
         .catch(function (err) {
           res.json({
